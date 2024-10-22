4ef0c1bad407fd79b4e6fc35e6b010c333b9df61
Corwin <30555096+Corwin-dev@users.noreply.github.com>
2024-10-22 05:27:47 -0700

    Update TerrainBlockRandomizer.java

--- src/main/java/corwin/terrainblockrandomizer/TerrainBlockRandomizer.java
+++ src/main/java/corwin/terrainblockrandomizer/TerrainBlockRandomizer.java
@@ -6,14 +6,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TerrainBlockRandomizer implements ModInitializer {
-	public static final String MOD_ID = "terrainblockrandomizer";
+    public static final String MOD_ID = "terrainblockrandomizer";
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    // This logger is used to write text to the console and the log file.
+    // It is considered best practice to use your mod id as the logger's name.
+    // That way, it's clear which mod wrote info, warnings, and errors.
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	@Override
+    @Override
         public void onInitialize() {
         // Assuming a config directory relative to your mod
         File configDir = new File(FabricLoader.getInstance().getConfigDir().toFile(), "terrainblockrandomizer");
