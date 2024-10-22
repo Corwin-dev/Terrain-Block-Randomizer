e8ac863dd1e32ec5e7d726bd387b502a27e6a385
Corwin <30555096+Corwin-dev@users.noreply.github.com>
2024-10-22 01:02:44 -0700

    Add files via upload

---
+++ src/client/java/corwin/terrainblockrandomizer/TerrainBlockRandomizerClient.java
@@ -0,0 +1,10 @@
+package corwin.terrainblockrandomizer;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TerrainBlockRandomizerClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
